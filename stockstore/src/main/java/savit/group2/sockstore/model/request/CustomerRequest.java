2b3e1108b1c32940d9f85ee41c197fc002184339
Rittam <thatv12122003@gmail.com>
2023-11-06 18:12:14 +0700

    Done customer

--- stockstore/src/main/java/savit/group2/sockstore/model/request/CustomerRequest.java
+++ stockstore/src/main/java/savit/group2/sockstore/model/request/CustomerRequest.java
@@ -1,26 +1,19 @@
 package savit.group2.sockstore.model.request;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
-import org.hibernate.validator.constraints.Range;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Past;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.AccessLevel;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.experimental.FieldDefaults;
-import savit.group2.sockstore.model.entity.Account;
 import savit.group2.sockstore.model.entity.Customer;
 
 @Getter
@@ -31,18 +24,24 @@ import savit.group2.sockstore.model.entity.Customer;
 @Component
 public class CustomerRequest {
   public UUID id;
-  public String name;
-  @Email
-  public String email;
-  @Pattern(regexp = "^(0[0-9]{9}|\\+84[0-9]{9})$", message = "Invalid Vietnamese phone number")
-  public String phone;
-  @NotNull
-  @Past(message = "Birthday must be in the past")
-  public LocalDate birthday;
-  @NotNull
-  public String address;
 
-  public Boolean deleted;
+  @NotBlank(message = "Vui lòng nhập trường này!")
+  @NotNull(message = "Vui lòng nhập trường này!")
+  String email;
+  @NotBlank(message = "Vui lòng nhập trường này!")
+  @NotNull(message = "Vui lòng nhập trường này!")
+  String name;
+
+  @NotBlank(message = "Vui lòng nhập trường này!")
+  @NotNull(message = "Vui lòng nhập trường này!")
+  String address;
+  @NotBlank(message = "Vui lòng nhập trường này!")
+  @NotNull(message = "Vui lòng nhập trường này!")
+  String phone;
+  @NotNull(message = "trường này không thể để trống")
+  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @PastOrPresent(message = "Ngày sinh không hợp lệ")
+  LocalDate birthDay;
 
   public Boolean status;
   @NotNull
@@ -51,19 +50,35 @@ public class CustomerRequest {
   @NotNull
 
   public int districtcode;
+  @NotNull
+
   private String fulladdress;
 
   public CustomerRequest(Customer customer) {
     this.id = customer.getId();
-    this.name = customer.getEmail();
+    this.name = customer.getName();
     this.email = customer.getEmail();
     this.phone = customer.getPhone();
-    this.birthday = customer.getBirthday();
+    this.birthDay = customer.getBirthday();
     this.address = customer.getAddress();
-    this.deleted = customer.getDeleted();
     this.status = customer.getStatus();
     this.wardcode = customer.getWardcode();
     this.districtcode = customer.getDistrictcode();
-    // this.status = customer.getStatus();
+    this.fulladdress = customer.getFulladdress();
+  }
+
+  public static Customer mapToRequest(CustomerRequest customerRequest) {
+    Customer cus = new Customer();
+    cus.setId(customerRequest.getId());
+    cus.setName(customerRequest.getName());
+    cus.setEmail(customerRequest.getEmail());
+    cus.setPhone(customerRequest.getPhone());
+    cus.setBirthday(customerRequest.getBirthDay());
+    cus.setAddress(customerRequest.getAddress());
+    cus.setStatus(customerRequest.getStatus());
+    cus.setWardcode(customerRequest.getWardcode());
+    cus.setDistrictcode(customerRequest.getDistrictcode());
+    cus.setFulladdress(customerRequest.getFulladdress());
+    return cus;
   }
 }
