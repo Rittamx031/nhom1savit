24d431cb5ad9a16de7f74cd1ecd9c3d02a6e8fc3
hieudangvietcode <106416768+hieudangvietcode@users.noreply.github.com>
2023-11-19 21:44:57 +0700

    hieuupv65

--- stockstore/hieulamrieng/Savis-Wenbanquanao/Savis-WebBanQuanAoo/src/main/java/edu/poly/DemoApplication.java
+++ stockstore/hieulamrieng/Savis-Wenbanquanao/Savis-WebBanQuanAoo/src/main/java/edu/poly/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class AssignmentApplication {
+public class DemoApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AssignmentApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
